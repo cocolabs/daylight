90b392a912d7d5ef9661b899bdb0f7e6ab37c4bf
Matthew Cain <yooksi@zoho.com>
2020-05-20 21:35:07 +0200

    Fix potential NPE error

--- src/main/java/io/yooksi/daylight/gui/GuiHandler.java
+++ src/main/java/io/yooksi/daylight/gui/GuiHandler.java
@@ -17,13 +17,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.Biomes;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Objects;
 
 @Mod.EventBusSubscriber
 public class GuiHandler {
@@ -47,8 +44,8 @@ public class GuiHandler {
 			if (cycle != null) {
 				cycle.updateAndDraw(world);
 			}
-			// Default to Plains biome for everything else
-			else Objects.requireNonNull(TimeCycle.getForBiome(Biomes.PLAINS)).updateAndDraw(world);
+			// Use default instance for everything else
+			else TimeCycle.getDefault().updateAndDraw(world);
 		}
 	}
 }
