09bf432ef90d145792fe492373d752a942ef7df3
Matthew Cain <yooksi@zoho.com>
2020-05-22 04:44:06 +0200

    Fix GuiHandler registration
    The registration process had following issues:
    
    - The class was being registered twice.
    - Registration annotation did not include modid.
    - SubscribeEvent was not annotating a static method.

--- src/main/java/io/yooksi/daylight/gui/GuiHandler.java
+++ src/main/java/io/yooksi/daylight/gui/GuiHandler.java
@@ -13,6 +13,7 @@
  */
 package io.yooksi.daylight.gui;
 
+import io.yooksi.daylight.Daylight;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.world.World;
@@ -22,11 +23,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.Nullable;
 
-@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber(modid = Daylight.MODID)
 public class GuiHandler {
 
 	@SubscribeEvent
-	public void onPreRenderOverlay(RenderGameOverlayEvent.Pre event) {
+	public static void onPreRenderOverlay(RenderGameOverlayEvent.Pre event) {
 
 		// Render along with ALL other other HUD elements
 		// otherwise we risk our GUI element rendering multiple times
